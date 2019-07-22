7ce25699b9c4262612dabff63c981b0ea31edf3b
Emmanuel Paternina <e.paternina@softd3v.com>
2019-07-22 12:59:57 -0500

    Add files via upload
    Included a string split, because I need to read license (PDF417) from a scanner and get the personal ID Number to look for in the Json File.

--- js/search_table.js
+++ js/search_table.js
@@ -2,8 +2,11 @@
 $(document).ready(function(){
     $("#myInput").on("keyup", function() {
       var value = $(this).val().toLowerCase();
+      var res = value.split("|");
+
       $("#myTable tr").filter(function() {
-        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        $(this).toggle($(this).text().toLowerCase().indexOf(res[0]) > -1)
+        console.log(res);
       });
     })
   });
